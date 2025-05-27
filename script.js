7501fd9e8ca868903aeb4da78dc885d467305824
Elcand <tjandraraharja99@gmail.com>
2025-05-27 14:23:21 +0700

    finish

--- script.js
+++ script.js
@@ -12,4 +12,25 @@ function calculate() {
   display.value = eval(display.value);
 }
 
+document.addEventListener("keydown", function (event) {
+  const key = event.key;
+  if (
+    (key >= "0" && key <= "9") ||
+    key === "." ||
+    key === "00" ||
+    key === "*" ||
+    key === "/" ||
+    key === "-" ||
+    key === "+"
+  ) {
+    numberInput(key);
+  }
 
+  if (key === "Enter") {
+    calculate();
+  }
+
+  if (key === "Backspace") {
+    clearDisplay();
+  }
+});
